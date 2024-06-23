5ff51b3cbaddb53bc0c4f821c97784eb72e46433
ghost-254 <nevilleotieno6@gmail.com>
2024-06-24 00:04:04 +0300

    new updates

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -27,7 +27,7 @@ const Photo = () => {
             priority
             quality={100}
             fill
-            alt=""
+            alt="profile picture"
             className="object-contain"
           />
         </motion.div>
